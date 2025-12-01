cb914014da041899fe4483975de11b953b478838
Modotti <luimodotti@gmail.com>
2025-12-01 09:27:01 -0300

    feat: native minor adjustments

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,16 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'astroplanner',
-  webDir: 'www'
+  appId: 'com.app.astroplanner',
+  appName: 'AstroPlanner',
+  webDir: 'www',
+  plugins: {
+    StatusBar: {
+      overlaysWebView: false,
+      backgroundColor: '#000000',
+      style: 'DARK'
+    }
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,89 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Platform, NavController } from '@ionic/angular';
 import { TargetCatalogService } from './core/services/target-catalog/target-catalog.service';
-import { AstroCoreService } from './core/services/astro-core/astro-core-service';
-import { PlanningService } from './core/services/planning/planning-service';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
-  providers: [AstroCoreService, PlanningService]
+  imports: [IonApp, IonRouterOutlet]
 })
 export class AppComponent {
-  constructor(targetCatalogService: TargetCatalogService, astroCoreService: AstroCoreService, planningService: PlanningService) {
+  @ViewChild(IonRouterOutlet, { static: true })
+  routerOutlet!: IonRouterOutlet;
+
+
+  constructor(
+    targetCatalogService: TargetCatalogService,
+    private platform: Platform,
+    private navCtrl: NavController
+  ) {
     document.body.classList.add('dark');
 
-/*
-    setTimeout(() => {
+    this.platform.ready().then(() => {
+      StatusBar.setOverlaysWebView({ overlay: false });
+      StatusBar.setBackgroundColor({ color: '#000000' });
+      StatusBar.setStyle({ style: Style.Dark });
+    });
 
-      //Busca de target
-      console.log('--------------------------------');
-      console.log('--------- BUSCA ----------------');
-      console.log('--------------------------------');
+    this.initializeApp();
+  }
 
-      const target = targetCatalogService.searchByName('Rosette')[0];
-      console.log(target);
-
-      //Dados target
-      console.log('--------------------------------');
-      console.log('--------- TARGET ---------------');
-      console.log('--------------------------------');
-
-      const location = {
-        latitude: -22.59170,
-        longitude: -50.42120,
-        elevationMeters: 600,
-      };
-
-      const ra = target.ra.split(' ');
-      const dec = target.dec.split(' ');
-      const m42 = {
-        name: target.familiarName,
-        raHours: astroCoreService.raHmsToHours(+ra[0], +ra[1], +ra[2]),
-        decDegrees: astroCoreService.decDmsToDegrees(+dec[0], +dec[1], +dec[2])
-      };
-
-      const now = new Date();
-      const time = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        21, 0, 0
-      );
-      const position = astroCoreService.getTargetHorizontalPosition(m42, time, location);
-
-      console.log('Altitude:', position.altitude);
-      console.log('Azimute:', position.azimuth);
-      console.log('Direção:', position.direction);
-
-      //Dados do dia
-      console.log('--------------------------------');
-      console.log('--------- DADOS DO DIA ---------');
-      console.log('--------------------------------');
-
-      const today = new Date();
-      const sky = astroCoreService.getDailySkyInfo(today, location);
-
-      console.log('--- Lua ---');
-      console.log('Fase:', sky.moon.phaseName);
-      console.log('Iluminação:', sky.moon.illuminationPercent.toFixed(1) + '%');
-      console.log('Nascer:', sky.moon.moonrise?.toLocaleTimeString() ?? '—');
-      console.log('Pôr:', sky.moon.moonset?.toLocaleTimeString() ?? '—');
-
-      console.log('--- Sol ---');
-      console.log('Nascer:', sky.sun.sunrise?.toLocaleTimeString() ?? '—');
-      console.log('Pôr:', sky.sun.sunset?.toLocaleTimeString() ?? '—');
-
-      //Planning
-      const plan = planningService.getCapturePlan(today, m42, location, {
-        minAltitudeDeg: 30,
-        stepMinutes: 30,
+  private initializeApp(): void {
+    this.platform.ready().then(() => {
+      App.addListener('backButton', ({ canGoBack }) => {
+        if (this.routerOutlet && this.routerOutlet.canGoBack()) {
+          this.navCtrl.back();
+        }
       });
-
-      console.log(plan);
-
-    }, 100);
-*/
+    });
   }
 }
